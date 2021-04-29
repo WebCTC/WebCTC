af7ec8819349e57403fe532c19bd70a6c40099fd
Kaiz <kaiz@kaiz.jp>
2021-04-30 02:39:11 +0900

    /api/内のスラッシュつけ忘れを修正(実際のURLの変更はなし)

--- src/main/java/org/webctc/router/api/ApiRouter.kt
+++ src/main/java/org/webctc/router/api/ApiRouter.kt
@@ -11,7 +11,7 @@ class ApiRouter : ExpressRouter() {
                         "http://${req.host}${req.uri}formations/\n" +
                         "http://${req.host}${req.uri}formations/<formationId>\n" +
                         "http://${req.host}${req.uri}formations/<formationId>/trains\n" +
-                        "http://${req.host}${req.uri}trains\n" +
+                        "http://${req.host}${req.uri}trains/\n" +
                         "http://${req.host}${req.uri}trains/<entityId>\n"
             )
         }
