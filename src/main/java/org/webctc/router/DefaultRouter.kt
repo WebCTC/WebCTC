dc8e30320e1c075c6c671868ab334d437c47eb9e
Kaiz <kaiz@kaiz.jp>
2021-05-01 16:57:45 +0900

    CTCページを実装

--- src/main/java/org/webctc/router/DefaultRouter.kt
+++ src/main/java/org/webctc/router/DefaultRouter.kt
@@ -1,11 +1,16 @@
 package org.webctc.router
 
 import express.ExpressRouter
+import express.utils.MediaType
+import jp.ngt.ngtlib.io.NGTFileLoader
+import net.minecraft.util.ResourceLocation
+import org.webctc.WebCTCCore
 
 class DefaultRouter : ExpressRouter() {
     init {
         get("/") { req, res ->
-            res.send("Hello World!")
+            val inputStream = NGTFileLoader.getInputStream(ResourceLocation(WebCTCCore.MODID, "html/index.html"))
+            res.streamFrom(0, inputStream, MediaType._html)
         }
     }
 }
