8adb020ef300043c1964266eabd73e7fda4754d2
Kaiz <kaiz@kaiz.jp>
2021-05-02 22:02:27 +0900

    SignalRouterにblockDirectionを追加

--- src/main/java/org/webctc/router/api/SignalRouter.kt
+++ src/main/java/org/webctc/router/api/SignalRouter.kt
@@ -3,6 +3,7 @@ package org.webctc.router.api
 import express.utils.MediaType
 import jp.ngt.ngtlib.util.NGTUtil
 import jp.ngt.rtm.electric.TileEntitySignal
+import jp.ngt.rtm.modelpack.modelset.ModelSetSignal
 import org.webctc.WebCTCCore
 import org.webctc.router.WebCTCRouter
 
@@ -27,7 +28,15 @@ fun TileEntitySignal.toMutableMap(): MutableMap<String, Any?> {
     jsonMap["pos"] = arrayOf(this.xCoord, this.yCoord, this.zCoord)
     jsonMap["rotation"] = this.rotation
     jsonMap["signalLevel"] = NGTUtil.getField(TileEntitySignal::class.java, this, "signalLevel")
+    jsonMap["signalType"] = getSignalTypes(this)
+    jsonMap["blockDirection"] = this.blockDirection
     jsonMap["modelname"] = this.modelName
 
     return jsonMap
+}
+
+fun getSignalTypes(tileEntitySignal: TileEntitySignal): Map<Int, Boolean> {
+    return ModelSetSignal.parseLightParts(tileEntitySignal.modelSet.config.lights).associate {
+        it.signalLevel to true
+    }
 }
