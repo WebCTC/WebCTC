34b7422c5cca039560b920b3f10f67b84a71f70d
Kaiz <kaiz@kaiz.jp>
2021-07-21 10:21:00 +0900

    外部からのアクセス用に表示するURLの設定を追加

--- src/main/java/org/webctc/WebCTCConfig.kt
+++ src/main/java/org/webctc/WebCTCConfig.kt
@@ -1,17 +1,24 @@
 package org.webctc
 
 import net.minecraftforge.common.config.Configuration
-import net.minecraftforge.common.config.Property
 
 class WebCTCConfig {
     companion object {
-        var portNumber: Int = 8080
+        var portNumber = 8080
+        var accessUrl = ""
+        var accessPort = 0
 
         fun preInit(cfg: Configuration) {
             cfg.load()
-            val portProperty: Property = cfg["Network", "port number", 8080]
+            val portProperty = cfg["Network", "port number", 8080]
             portProperty.comment = "Port number used by WebCTC"
+            val accessUrlProperty = cfg["Network", "access url", ""]
+            accessUrlProperty.comment = "if null, display your machine IP address"
+            val accessPortProperty = cfg["Network", "access port", 0]
+            accessPortProperty.comment = "if 0, display webctc port number"
             this.portNumber = portProperty.int
+            this.accessUrl = accessUrlProperty.string
+            this.accessPort = accessPortProperty.int
             cfg.save()
         }
     }
