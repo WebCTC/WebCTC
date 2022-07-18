8f8091d082fe07ab22b87ba3bbc6b20de36598f4
Kaiz <kaiz@kaiz.jp>
2022-07-18 21:06:26 +0900

    ドアの開閉状態を色で表示

--- src/main/resources/assets/webctc/html/map.js
+++ src/main/resources/assets/webctc/html/map.js
@@ -66,190 +66,194 @@ async function updateRail(svg, ws = false) {
           })
       }
     })
-    .then(await fetch(SIGNAL_DATA_URL)
-      .then(res => res.json())
-      .then(json => {
-        let signalGroup = document.getElementById("signals")
-        let updateList = Array.from(document.querySelectorAll("[id^='signal,']"))
+    .then(() => {
+      fetch(SIGNAL_DATA_URL)
+        .then(res => res.json())
+        .then(json => {
+          let signalGroup = document.getElementById("signals")
+          let updateList = Array.from(document.querySelectorAll("[id^='signal,']"))
 
-        json.forEach(signal => {
-          let pos = signal["pos"]
-          let rotation = signal["rotation"]
-          let blockDirection = signal["blockDirection"]
-          let cos = Math.cos((270 + rotation) * (Math.PI / 180))
-          let sin = Math.sin((270 + rotation) * (Math.PI / 180))
-          let signalLevel = signal["signalLevel"]
-          let p = (blockDirection * 90 - rotation + 360) % 360
-          let fixX = (45 < p && p < 135) ? 3 : ((225 < p && p < 315) ? -3 : 0);
+          json.forEach(signal => {
+            let pos = signal["pos"]
+            let rotation = signal["rotation"]
+            let blockDirection = signal["blockDirection"]
+            let cos = Math.cos((270 + rotation) * (Math.PI / 180))
+            let sin = Math.sin((270 + rotation) * (Math.PI / 180))
+            let signalLevel = signal["signalLevel"]
+            let p = (blockDirection * 90 - rotation + 360) % 360
+            let fixX = (45 < p && p < 135) ? 3 : ((225 < p && p < 315) ? -3 : 0);
 
-          let posX = pos[0]
-          let posZ = pos[2]
-          let id = "signal," + pos[0] + "," + pos[2] + ","
+            let posX = pos[0]
+            let posZ = pos[2]
+            let id = "signal," + pos[0] + "," + pos[2] + ","
 
-          let circle = this.createSignalCircle(posX, pos[1], posZ, signalLevel, fixX)
+            let circle = this.createSignalCircle(posX, pos[1], posZ, signalLevel, fixX)
 
-          let group = document.getElementById(id)
-          if (group == null) {
-            group = document.createElementNS('http://www.w3.org/2000/svg', 'g')
-            group.id = id
-
-            let line = createLineFromComponent(
-              posX + 1.5 * cos, posZ - 1.5 * sin,
-              2.5 * cos, -2.5 * sin
-            )
-            line.setAttribute('stroke', 'lightgray');
-            line.setAttribute('stroke-width', '0.5px');
-            line.setAttribute('name', 'verticalLine');
-            group.appendChild(line);
-
-            let baseLine = createLineFromComponent(
-              posX + 4 * cos + 1.5 * sin, posZ - 4 * sin + 1.5 * cos,
-              -3.0 * sin, -3.0 * cos
-            )
-            baseLine.setAttribute('stroke', 'lightgray');
-            baseLine.setAttribute('stroke-width', '0.5px');
-            baseLine.setAttribute('name', 'horizontalLine');
-            group.appendChild(baseLine);
-
-            group.appendChild(circle)
-
-            signalGroup.appendChild(group);
-          } else {
-            updateList = updateList.filter(n => n !== group)
-            let minus = 0
-            let last;
-            let circleArray = Array.from(group.getElementsByTagName('circle'))
-            let support
-            if ((support = group.getElementsByTagName('g')[0]) == null) {
-              support = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-              group.appendChild(support);
-            }
-
-            if (support.innerHTML !== "") {
-              support.innerHTML = ""
-            }
-
-            let circleCache = circleArray.find(c => Number(c.getAttribute("yCoord")) === Number(pos[1]))
-            if (circleCache == null) {
-              circleArray.push(circle)
-            } else {
-              let color = getSignalColor(signalLevel)
-              if (circleCache.getAttribute('fill') !== color) {
-                circleCache.setAttribute('fill', color)
-              }
-            }
-
-            if (circleArray.length > 1) {
-              circleArray
-                .sort((a, b) => Number(a.getAttribute("yCoord")) < Number(b.getAttribute("yCoord")) ? -1 : 1)
-                .forEach((value, index) => {
-                  let fixX = Number(value.getAttribute("fix"))
-                  if (fixX === 0 && index === 0) {
-                    minus += (index - last) ? -1.5 : +1.5
-                    last = index
-                    fixX = Number(circleArray[index + 1].getAttribute("fix")) * -1
-                  } else if (fixX !== 0 && index !== 0) {
-                    minus += (index - last) ? -1.5 : +1.5
-                    last = index
-                    fixX = 0;
-                  } else if (fixX !== 0) {
-                    minus += (index - last) ? -1.5 : +1.5
-                    last = index
-                  } else if (index - last) {
-                    minus -= 1.0
-                  }
-                  let cx = posX - (3.5 * index + minus) * cos - fixX * sin
-                  let cy = posZ + (3.5 * index + minus) * sin - fixX * cos
-                  value.setAttribute('cx', cx)
-                  value.setAttribute('cy', cy)
-                  group.appendChild(value)
-
-                  if (fixX !== 0 || fixX === 0 && index === 0) {
-                    let polyline = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
-                    polyline.setAttribute('points',
-                      (cx + 1.5 * cos) + "," + (cy - 1.5 * sin) + " " +
-                      (cx + 3 * cos) + "," + (cy - 3 * sin) + " " +
-                      (cx + 3 * cos + fixX * sin) + "," + (cy - 3 * sin + fixX * cos)
-                    );
-                    polyline.setAttribute('stroke', 'lightgray');
-                    polyline.setAttribute('stroke-width', '0.5px');
-                    polyline.setAttribute('name', 'holizonalLine');
-                    polyline.setAttribute('fill', 'none');
-                    support.appendChild(polyline);
-                  }
-                })
-            }
-            Array.from(group.getElementsByTagName('line'))
-              .filter(line => line.getAttribute("name") === "verticalLine")
-              .find(line => {
-                line.setAttribute('x1', posX + (1.5 - 2.5 * circleArray.length - minus) * cos);
-                line.setAttribute('y1', posZ - (1.5 - 2.5 * circleArray.length - minus) * sin);
-              })
-          }
-        });
-
-        updateList.forEach(n => n.remove());
-      }))
-    .then(await fetch(FORMATION_DATA_URL)
-      .then(res => res.json())
-      .then(json => {
-        let formationGroup = document.getElementById("formations")
-        let updateList = Array.from(document.querySelectorAll("[id^='formation,']"))
-
-        json.forEach(formation => {
-          if (formation != null && formation["controlCar"] != null) {
-            let id = "formation," + formation["id"] + ","
             let group = document.getElementById(id)
-
             if (group == null) {
               group = document.createElementNS('http://www.w3.org/2000/svg', 'g')
               group.id = id
-              formationGroup.appendChild(group)
+
+              let line = createLineFromComponent(
+                posX + 1.5 * cos, posZ - 1.5 * sin,
+                2.5 * cos, -2.5 * sin
+              )
+              line.setAttribute('stroke', 'lightgray');
+              line.setAttribute('stroke-width', '0.5px');
+              line.setAttribute('name', 'verticalLine');
+              group.appendChild(line);
+
+              let baseLine = createLineFromComponent(
+                posX + 4 * cos + 1.5 * sin, posZ - 4 * sin + 1.5 * cos,
+                -3.0 * sin, -3.0 * cos
+              )
+              baseLine.setAttribute('stroke', 'lightgray');
+              baseLine.setAttribute('stroke-width', '0.5px');
+              baseLine.setAttribute('name', 'horizontalLine');
+              group.appendChild(baseLine);
+
+              group.appendChild(circle)
+
+              signalGroup.appendChild(group);
             } else {
               updateList = updateList.filter(n => n !== group)
+              let minus = 0
+              let last;
+              let circleArray = Array.from(group.getElementsByTagName('circle'))
+              let support
+              if ((support = group.getElementsByTagName('g')[0]) == null) {
+                support = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+                group.appendChild(support);
+              }
+
+              if (support.innerHTML !== "") {
+                support.innerHTML = ""
+              }
+
+              let circleCache = circleArray.find(c => Number(c.getAttribute("yCoord")) === Number(pos[1]))
+              if (circleCache == null) {
+                circleArray.push(circle)
+              } else {
+                let color = getSignalColor(signalLevel)
+                if (circleCache.getAttribute('fill') !== color) {
+                  circleCache.setAttribute('fill', color)
+                }
+              }
+
+              if (circleArray.length > 1) {
+                circleArray
+                  .sort((a, b) => Number(a.getAttribute("yCoord")) < Number(b.getAttribute("yCoord")) ? -1 : 1)
+                  .forEach((value, index) => {
+                    let fixX = Number(value.getAttribute("fix"))
+                    if (fixX === 0 && index === 0) {
+                      minus += (index - last) ? -1.5 : +1.5
+                      last = index
+                      fixX = Number(circleArray[index + 1].getAttribute("fix")) * -1
+                    } else if (fixX !== 0 && index !== 0) {
+                      minus += (index - last) ? -1.5 : +1.5
+                      last = index
+                      fixX = 0;
+                    } else if (fixX !== 0) {
+                      minus += (index - last) ? -1.5 : +1.5
+                      last = index
+                    } else if (index - last) {
+                      minus -= 1.0
+                    }
+                    let cx = posX - (3.5 * index + minus) * cos - fixX * sin
+                    let cy = posZ + (3.5 * index + minus) * sin - fixX * cos
+                    value.setAttribute('cx', cx)
+                    value.setAttribute('cy', cy)
+                    group.appendChild(value)
+
+                    if (fixX !== 0 || fixX === 0 && index === 0) {
+                      let polyline = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
+                      polyline.setAttribute('points',
+                        (cx + 1.5 * cos) + "," + (cy - 1.5 * sin) + " " +
+                        (cx + 3 * cos) + "," + (cy - 3 * sin) + " " +
+                        (cx + 3 * cos + fixX * sin) + "," + (cy - 3 * sin + fixX * cos)
+                      );
+                      polyline.setAttribute('stroke', 'lightgray');
+                      polyline.setAttribute('stroke-width', '0.5px');
+                      polyline.setAttribute('name', 'holizonalLine');
+                      polyline.setAttribute('fill', 'none');
+                      support.appendChild(polyline);
+                    }
+                  })
+              }
+              Array.from(group.getElementsByTagName('line'))
+                .filter(line => line.getAttribute("name") === "verticalLine")
+                .find(line => {
+                  line.setAttribute('x1', posX + (1.5 - 2.5 * circleArray.length - minus) * cos);
+                  line.setAttribute('y1', posZ - (1.5 - 2.5 * circleArray.length - minus) * sin);
+                })
             }
-            json = formation["controlCar"]
-            group.innerHTML = ""
-            let pos = json["pos"]
-            let posX = pos[0]
-            let posZ = pos[2]
+          });
 
-            let rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-            rect.setAttribute('x', String(posX - 3))
-            rect.setAttribute('y', String(posZ - 3))
-            rect.setAttribute('width', "6px")
-            rect.setAttribute('height', "6px")
-            rect.setAttribute('fill', "YELLOW")
-            rect.setAttribute('stroke', "GRAY")
-            group.appendChild(rect)
+          updateList.forEach(n => n.remove());
+        })
+      fetch(FORMATION_DATA_URL)
+        .then(res => res.json())
+        .then(json => {
+          let formationGroup = document.getElementById("formations")
+          let updateList = Array.from(document.querySelectorAll("[id^='formation,']"))
 
-            let text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-            text.textContent = (json["name"] === "no_name" ? "" : json["name"] + " ") + json["driver"]
-            text.setAttribute('x', String(posX + 5))
-            text.setAttribute('y', String(posZ + 3))
-            text.setAttribute('font-size', "8")
-            text.setAttribute('font-weight', "bold")
-            text.setAttribute('fill', "white")
-            text.setAttribute('stroke-width', "0.35px")
-            group.appendChild(text);
+          json.forEach(formation => {
+            if (formation != null && formation["controlCar"] != null) {
+              let id = "formation," + formation["id"] + ","
+              let group = document.getElementById(id)
+
+              if (group == null) {
+                group = document.createElementNS('http://www.w3.org/2000/svg', 'g')
+                group.id = id
+                formationGroup.appendChild(group)
+              } else {
+                updateList = updateList.filter(n => n !== group)
+              }
+              json = formation["controlCar"]
+              group.innerHTML = ""
+              let pos = json["pos"]
+              let posX = pos[0]
+              let posZ = pos[2]
+
+              let rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+              rect.setAttribute('x', String(posX - 3))
+              rect.setAttribute('y', String(posZ - 3))
+              rect.setAttribute('width', "6px")
+              rect.setAttribute('height', "6px")
+              rect.setAttribute('fill', json["trainStateData"][4] === 0 ? "YELLOW" : "DARKGRAY")
+              rect.setAttribute('stroke', "GRAY")
+              group.appendChild(rect)
+
+              let text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+              text.textContent = (json["name"] === "no_name" ? "" : json["name"] + " ") + json["driver"]
+              text.setAttribute('x', String(posX + 5))
+              text.setAttribute('y', String(posZ + 3))
+              text.setAttribute('font-size', "8")
+              text.setAttribute('font-weight', "bold")
+              text.setAttribute('fill', "white")
+              text.setAttribute('stroke-width', "0.35px")
+              group.appendChild(text);
 
 
-            let bbox = text.getBBox();
-            let background = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-            background.setAttribute('x', String(bbox.x))
-            background.setAttribute('y', String(bbox.y))
-            background.setAttribute('width', String(bbox.width))
-            background.setAttribute('height', String(bbox.height))
-            background.setAttribute('fill', "#000000")
-            background.setAttribute('fill-opacity', "0.5")
-            background.setAttribute('rx', "1px")
-            background.setAttribute('ry', "1px")
-            group.insertBefore(background, text);
-          }
-        });
+              let bbox = text.getBBox();
+              let background = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+              background.setAttribute('x', String(bbox.x))
+              background.setAttribute('y', String(bbox.y))
+              background.setAttribute('width', String(bbox.width))
+              background.setAttribute('height', String(bbox.height))
+              background.setAttribute('fill', "#000000")
+              background.setAttribute('fill-opacity', "0.5")
+              background.setAttribute('rx', "1px")
+              background.setAttribute('ry', "1px")
+              group.insertBefore(background, text);
+            }
+          });
 
-        updateList.forEach(n => n.remove());
-      }))
+          updateList.forEach(n => n.remove());
+        })
+        .catch(err => {
+        })
+    })
 }
 
 function updateRailState(svg, json) {
