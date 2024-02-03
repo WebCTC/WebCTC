b4b63e4e93d406ed4be90baa0ed8a2a2ab326e94
Kaiz <kaiz@kaiz.jp>
2024-02-03 22:08:23 +0900

    進路予約

--- mc/src/main/kotlin/org/webctc/railgroup/RailGroupData.kt
+++ mc/src/main/kotlin/org/webctc/railgroup/RailGroupData.kt
@@ -3,7 +3,6 @@ package org.webctc.railgroup
 import jp.ngt.rtm.rail.TileEntityLargeRailCore
 import kotlinx.uuid.UUID
 import net.minecraft.nbt.NBTTagCompound
-import net.minecraft.nbt.NBTTagList
 import net.minecraft.server.MinecraftServer
 import net.minecraft.world.WorldSavedData
 import org.webctc.common.types.railgroup.RailGroup
@@ -11,32 +10,30 @@ import org.webctc.common.types.railgroup.RailGroup
 class RailGroupData(mapName: String) : WorldSavedData(mapName) {
     override fun readFromNBT(nbt: NBTTagCompound) {
         railGroupList.clear()
-        val tagList = nbt.getTagList("RailGroupData", 10)
-        (0 until tagList.tagCount())
-            .map { tagList.getCompoundTagAt(it) }
-            .mapTo(railGroupList) { RailGroup.readFromNBT(it) }
+        nbt.getTagList("RailGroupData", 10).toList()
+            .mapTo(railGroupList, RailGroup::readFromNBT)
     }
 
     override fun writeToNBT(nbt: NBTTagCompound) {
-        val tagList = NBTTagList()
-        railGroupList.map { it.writeToNBT() }.forEach { tagList.appendTag(it) }
-        nbt.setTag("RailGroupData", tagList)
+        railGroupList
+            .map(RailGroup::writeToNBT)
+            .toNBTTagList()
+            .let { nbt.setTag("RailGroupData", it) }
     }
 
     companion object {
         val railGroupList = mutableListOf<RailGroup>()
+        val lockList = mutableMapOf<UUID, String>()
 
         @JvmStatic
         fun setSignal(uuid: UUID, signal: Int) {
             val world = MinecraftServer.getServer().entityWorld
-            railGroupList
-                .find { it.uuid == uuid }
-                ?.let { rg ->
-                    rg.railPosList
-                        .map { world.getTileEntity(it.x, it.y, it.z) }
-                        .filterIsInstance<TileEntityLargeRailCore>()
-                        .forEach { it.signal = signal }
-                }
+            findRailGroup(uuid)?.let { rg ->
+                rg.railPosList
+                    .map { world.getTileEntity(it.x, it.y, it.z) }
+                    .filterIsInstance<TileEntityLargeRailCore>()
+                    .forEach { it.signal = signal }
+            }
         }
 
         @JvmStatic
@@ -46,12 +43,69 @@ class RailGroupData(mapName: String) : WorldSavedData(mapName) {
 
         @JvmStatic
         fun isTrainOnRail(uuid: UUID): Boolean {
-            return railGroupList.find { it.uuid == uuid }?.isTrainOnRail() ?: false
+            return findRailGroup(uuid)?.isTrainOnRail() ?: false
         }
 
         @JvmStatic
         fun isTrainOnRail(uuid: String): Boolean {
             return isTrainOnRail(UUID(uuid))
+        }
+
+        @JvmStatic
+        fun reserve(uuid: UUID, key: String) {
+            findRailGroup(uuid)?.let { railGroup ->
+                if (!railGroup.isTrainOnRail() &&
+                    !lockList.containsKey(uuid) &&
+                    lockList.keys
+                        .mapNotNull(::findRailGroup)
+                        .none { it.railPosList.any { pos -> pos in railGroup.railPosList } }
+                ) {
+                    lockList[uuid] = key
+                }
+            }
+        }
+
+        @JvmStatic
+        fun reserve(uuid: String, key: String) {
+            reserve(UUID(uuid), key)
+        }
+
+        @JvmStatic
+        fun release(uuid: UUID, key: String) {
+            findRailGroup(uuid)?.let {
+                if (lockList[uuid] == key) {
+                    lockList.remove(uuid)
+                }
+            }
+        }
+
+        @JvmStatic
+        fun release(uuid: String, key: String) {
+            release(UUID(uuid), key)
+        }
+
+        @JvmStatic
+        fun forceRelease(uuid: UUID) {
+            lockList.remove(uuid)
+        }
+
+        @JvmStatic
+        fun forceRelease(uuid: String) {
+            forceRelease(UUID(uuid))
+        }
+
+        @JvmStatic
+        fun isReserved(uuid: UUID, key: String): Boolean {
+            return lockList[uuid] == key
+        }
+
+        @JvmStatic
+        fun isReserved(uuid: String, key: String): Boolean {
+            return isReserved(UUID(uuid), key)
+        }
+
+        private fun findRailGroup(uuid: UUID): RailGroup? {
+            return railGroupList.find { it.uuid == uuid }
         }
     }
 }
--- mc/src/main/kotlin/org/webctc/webctcex/utils/RailGroupManager.kt
+++ mc/src/main/kotlin/org/webctc/webctcex/utils/RailGroupManager.kt
@@ -25,5 +25,45 @@ class RailGroupManager {
         fun isTrainOnRail(uuid: String): Boolean {
             return isTrainOnRail(UUID.fromString(uuid))
         }
+
+        @JvmStatic
+        fun reserve(uuid: UUID, key: String) {
+            RailGroupData.reserve(uuid.toKotlinUUID(), key)
+        }
+
+        @JvmStatic
+        fun reserve(uuid: String, key: String) {
+            reserve(UUID.fromString(uuid), key)
+        }
+
+        @JvmStatic
+        fun release(uuid: UUID, key: String) {
+            RailGroupData.release(uuid.toKotlinUUID(), key)
+        }
+
+        @JvmStatic
+        fun release(uuid: String, key: String) {
+            release(UUID.fromString(uuid), key)
+        }
+
+        @JvmStatic
+        fun forceRelease(uuid: UUID) {
+            RailGroupData.forceRelease(uuid.toKotlinUUID())
+        }
+
+        @JvmStatic
+        fun forceRelease(uuid: String) {
+            forceRelease(UUID.fromString(uuid))
+        }
+
+        @JvmStatic
+        fun isReserved(uuid: UUID, key: String): Boolean {
+            return RailGroupData.isReserved(uuid.toKotlinUUID(), key)
+        }
+
+        @JvmStatic
+        fun isReserved(uuid: String, key: String): Boolean {
+            return isReserved(UUID.fromString(uuid), key)
+        }
     }
 }
