04155931bea27c555d42d4375b58c6c8463345a4
Kaiz <kaiz@kaiz.jp>
2026-02-01 23:02:55 +0900

    1.0.0

--- mc/src/main/kotlin/org/webctc/WebCTCCore.kt
+++ mc/src/main/kotlin/org/webctc/WebCTCCore.kt
@@ -188,7 +188,7 @@ class WebCTCCore {
 
     companion object {
         const val MODID = "webctc"
-        const val VERSION = "1.0.0-rc.1"
+        const val VERSION = "1.0.0"
 
         val IN_CHAT_LOGO = "${GRAY}[${GREEN}Web${WHITE}CTC${GRAY}]"
 
